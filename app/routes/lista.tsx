cd8d63366a3adb5a38d03a0eb4e4bbc3967dab25
Kalliu Brasil <kalliub@gmail.com>
2023-07-30 22:25:20 -0300

    fix: removing unused variables

--- app/routes/lista.tsx
+++ app/routes/lista.tsx
@@ -1,15 +1,13 @@
-import { Box, GlobalStyles, Grid, Typography } from "@mui/material";
+import { Grid } from "@mui/material";
 import type { LoaderFunction } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Link, Outlet, useLoaderData, useLocation } from "@remix-run/react";
+import { Outlet, useLoaderData, useLocation } from "@remix-run/react";
 import { useMemo } from "react";
 import { getProductsCategory } from "api/sheet.server";
 import CategoryList from "components/CategoryList";
 import Navbar, { NAVBAR_HEIGHT } from "components/Navbar";
-import PageTitle from "components/PageTitle";
 import getJson from "config/sheetFromCSV";
 import { ListProvider } from "context/ListContext";
-import { palette } from "themes/palette.theme";
 
 export const loader: LoaderFunction = async () => {
   const listData = await getJson();
