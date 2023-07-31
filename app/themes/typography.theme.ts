29479e0f4e051c5d8ae9a7bfe16a7ee5795f0a93
Kalliu Brasil <kalliub@gmail.com>
2023-07-31 01:19:42 -0300

    feat: Creating product list layout

--- app/themes/typography.theme.ts
+++ app/themes/typography.theme.ts
@@ -12,56 +12,67 @@ export const typographyVariants = {
     fontWeight: 600,
     fontSize: "2.5rem",
     lineHeight: "3.75rem",
+    color: "white",
   },
   h2: {
     fontWeight: 600,
     fontSize: "2rem",
     lineHeight: "3rem",
+    color: "white",
   },
   h3: {
     fontWeight: 600,
     fontSize: "1.5rem",
     lineHeight: "2.25rem",
+    color: "white",
   },
   h4: {
     fontWeight: 600,
     fontSize: "1.25rem",
     lineHeight: "1.875rem",
+    color: "white",
   },
   h5: {
     fontWeight: 600,
     fontSize: "1rem",
     lineHeight: "1.5rem",
+    color: "white",
   },
   subtitle1: {
     fontWeight: 600,
     fontSize: "0.875rem",
     lineHeight: "1.25rem",
+    color: "white",
   },
   subtitle2: {
     fontWeight: 600,
     fontSize: "0.75rem",
     lineHeight: "1.125rem",
+    color: "white",
   },
   caption: {
     fontWeight: 500,
     fontSize: "0.75rem",
     lineHeight: "1.125rem",
+    color: "white",
   },
   body1: {
     fontWeight: 400,
     fontSize: "1rem",
     lineHeight: "1.5rem",
+    color: "white",
   },
   body2: {
     fontWeight: 400,
     fontSize: "0.875rem",
     lineHeight: "1.25rem",
+    color: "white",
   },
   body3: {
     fontWeight: 400,
     fontSize: "0.75rem",
     lineHeight: "1.125rem",
+    color: "white",
   },
 };
 
--- app/utils/formatters.ts
+++ app/utils/formatters.ts
@@ -9,3 +9,41 @@ export const pageTitle = (page?: string): string => {
   if (page) return `${page} | ${projectName}`;
   return projectName;
 };
+
+/**
+ * Slice the string if it's length is bigger than `size` and then add ellipsis.
+ *
+ * @param text
+ * The text to be sliced.
+ *
+ * @param size
+ * The limit length of `text`.
+ * If it doesn't reach this length, `text` will remain unchanged.
+ *
+ * @Returns
+ * Sliced (or not) string.
+ *
+ * @Example
+ * `sliceStringBiggerThan("endeavor", 10)`
+ *
+ *  Returns: `"pneumon..."`
+ */
+export const sliceStringBiggerThan = (
+  text: string | undefined,
+  size: number | undefined
+): string => {
+  if (!text) {
+    return "";
+  }
+
+  if (size && text.length > size) return `${text.slice(0, size - 3).trim()}...`;
+
+  return text;
+};
+
+export const formatCurrency = (value: number) => {
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(value);
+};
