14d618c27328e379a9f36963ad0604a848717be4
Kalliu Brasil <kalliub@gmail.com>
2023-08-10 00:02:25 -0300

    chore: reapplying Netlify configs to deploy

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,14 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  // server:
-  //   process.env.NETLIFY || process.env.NETLIFY_LOCAL
-  //     ? "./server.ts"
-  //     : undefined,
-  // serverBuildPath: ".netlify/functions-internal/server.js",
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
+  server:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? "./server.ts"
+      : undefined,
+  serverBuildPath: ".netlify/functions-internal/server.js",
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  publicPath: "/build/",
   future: {
     // v2_errorBoundary: true,
     // v2_meta: true,
